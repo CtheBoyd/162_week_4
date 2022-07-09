2ddb89cd37af5773e162521f35eee4684988b200
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-08 23:09:56 -0700

    update

--- bin_except.py
+++ bin_except.py
@@ -7,24 +7,20 @@ class TargetNotFound(Exception):
     """exception class"""
     pass
 
-class BinarySearch:
 
 
-    def __init__(self, binary_search):
-        self.binary_search = binary_search
+def bin_except(a_list, target):
+    """Binary search function with call for exception class when target not on list."""
 
+    first = 0
+    last = len(a_list) - 1
+    while first <= last:
+        middle = (first + last) // 2
+        if a_list[middle] == target:
+            return middle
+        if a_list[middle] > target:
+            last = middle - 1
+        else:
+            first = middle + 1
 
-    def bin_except(a_list, target):
-
-        first = 0
-        last = len(a_list) - 1
-        while first <= last:
-            middle = (first + last) // 2
-            if a_list[middle] == target:
-                return middle
-            if a_list[middle] > target:
-                last = middle - 1
-            else:
-                first = middle + 1
-
-        return TargetNotFound
+    return TargetNotFound
