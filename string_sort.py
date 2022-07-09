ef804bdea72f10838c01b329dcea3272ec80496b
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-09 15:19:59 -0700

    update

--- string_sort.py
+++ string_sort.py
@@ -4,7 +4,8 @@
 # Description: Case-insensitive string insertion sort
 #
 
-#a_list = ['Zebra', 'apple', 'maRker', 'marble']
+a_list = ['Zebra', 'apple', 'maRker', 'marble']
+#a_list = ['zebra', 'apple', 'marker', 'marble']
 def string_sort():
     """
     Sorts a_list in ascending order
@@ -12,12 +13,12 @@ def string_sort():
     for index in range(1, len(a_list)):
         value = a_list[index]
         pos = index - 1
-        while pos >= 0 and value < a_list[pos]:
+        while pos >= 0 and a_list[pos].lower() > value.lower():
             a_list[pos + 1] = a_list[pos]
-            pos = pos - 1
+            pos -= 1
         a_list[pos + 1] = value
-        a_list.sort(key=lambda x: x.lower())
 
-    #print(a_list)
 
-#string_sort()
+    print(a_list)
+
+string_sort()
