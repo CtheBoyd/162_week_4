6feec5406c05fb0e82ffcf1bfc4c986361bca1e9
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-09 18:27:00 -0700

    update

---
+++ sorts_count.py
@@ -0,0 +1,48 @@
+# Author: Christopher Boyd
+# GitHub username: CtheBoyd
+# Date: 7/9/2022
+# Description: calculates box volume and returns box list sorted by volume.
+#
+
+
+def bubble_count(a_list):
+  comp, exch = 0, 0
+  """
+  Sorts a_list in ascending order
+  """
+  for pass_num in range(len(a_list) - 1):
+    for index in range(len(a_list) - 1 - pass_num):
+      comp += 1
+      if a_list[index] > a_list[index + 1]:
+        exch += 1
+        temp = a_list[index]
+        a_list[index] = a_list[index + 1]
+        a_list[index + 1] = temp
+
+  return a_list, comp, exch
+
+
+def insertion_sort(a_list):
+  comp, exch = 0, 0
+
+  """
+  Sorts a_list in ascending order
+  """
+  for index in range(1, len(a_list)):
+    value = a_list[index]
+    pos = index - 1
+    comp += 1
+    while pos >= 0 and a_list[pos] > value:
+      a_list[pos + 1] = a_list[pos]
+      pos -= 1
+    a_list[pos+1] = value
+    exch += 1
+
+  return a_list, comp, exch
+
+
+
+
+#a_list =[2, 4, 6, 3]
+#print(bubble_count(a_list))
+#print(insertion_sort(a_list))
